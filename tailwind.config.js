2a1144b850e3f57d7151e62929a535de46db0767
ViniJesus <vini.silva1342@gmail.com>
2025-03-24 20:24:10 -0300

    WIP: FrontEnd com Tailwind | gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#00072D",
+        secondary: "#0E6BA8",
+        text_primary: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
